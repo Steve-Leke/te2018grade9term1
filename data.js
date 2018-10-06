be2dc0579f70cdfd02df8453a49942c0d9d0459a
Steve Leke <stevel@csedge.org>
2018-10-06 13:19:04 -0700

    I changed stuff
    I changed stuff

--- data.js
+++ data.js
@@ -1,5 +1,17 @@
-// Example
-let message = "Hello World!";
-console.log(message);
-
 // Type your code below!
+let donald = 6 ;
+console.log(donald);
+let cat = 8 ;
+console.log(cat);
+let baby = 5 ;
+console.log(baby);
+let daddy = "Papy" ;
+console.log(donald);
+let donald = 6 ;
+console.log(donald);
+let donald = 6 ;
+console.log(donald);
+let donald = 6 ;
+console.log(donald);
+let donald = 6 ;
+console.log(donald);
